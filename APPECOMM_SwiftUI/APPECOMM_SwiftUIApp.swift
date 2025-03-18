092f9e3d79dcc01b3e9af846084543d57b5a0526
EmerBV <emersmc@hotmail.com>
2025-03-18 14:21:05 +0100

    Initial Commit

---
+++ APPECOMM_SwiftUI/APPECOMM_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  APPECOMM_SwiftUIApp.swift
+//  APPECOMM_SwiftUI
+//
+//  Created by Emerson Balahan Varona on 18/3/25.
+//
+
+import SwiftUI
+
+@main
+struct APPECOMM_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
